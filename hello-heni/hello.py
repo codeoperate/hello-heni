3ef82a4ed5f166609114a19e9402d20301643773
Daniel Dunbar <daniel.pj.dunbar@gmail.com>
2022-10-28 06:30:21 +0100

    Show memory limit

--- hello-heni/hello.py
+++ hello-heni/hello.py
@@ -1,3 +1,6 @@
+"""
+This is a heni challenge answer: hello
+"""
 import json
 
 def lambda_handler(event, context):
@@ -5,4 +8,5 @@ def lambda_handler(event, context):
     This is the entry point for the lambda
     """
     print("Received event: " + json.dumps(event, indent=2))
+    print(f"Memory limit MB for lambda: {context.memory_limit_in_mb}")
     return "Hello"
