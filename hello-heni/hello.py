c548874c2fea002b03532d684042310746687c72
Daniel Dunbar <daniel.pj.dunbar@gmail.com>
2022-10-28 06:15:24 +0100

    Fixing pylint

--- hello-heni/hello.py
+++ hello-heni/hello.py
@@ -1,5 +1,8 @@
 import json
 
 def lambda_handler(event, context):
+    """
+    This is the entry point for the lambda
+    """
     print("Received event: " + json.dumps(event, indent=2))
     return "Hello"
